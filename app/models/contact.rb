4d86093d1f69e01fc561236f1758e42c8352e36d
Hans Yabao <hyabao0518@yahoo.com>
2022-07-17 13:07:50 +0000

    Added form validations to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,7 @@
+#model file provides the blueprint for the objects
 class Contact < ActiveRecord::Base
-    
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :comments, presence: true
 end 
 
-#model file provides the blueprint for the objects
